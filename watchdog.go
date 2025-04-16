731184d220c5b5e852ae9586c50512b1a0510977
Marek Jančiar <digraph.pace_0f@icloud.com>
2025-04-16 17:51:16 +0200

    Add service termination to watchdog

--- watchdog.go
+++ watchdog.go
@@ -16,6 +16,11 @@ func main() {
 			cmd := exec.Command("kubectl", "delete", "job", "ollama-runner")
 			err = cmd.Start()
 			err = cmd.Wait()
+
+			cmd = exec.Command("kubectl", "delete", "service", "ollama-runner")
+			err = cmd.Start()
+			err = cmd.Wait()
+
 			os.Exit(1)
 		}
 		time.Sleep(time.Minute)
